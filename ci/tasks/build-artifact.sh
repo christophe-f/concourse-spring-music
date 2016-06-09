76ae6e97840ef9f16ed78c3234a194ea0dc51ae6
christophe-f <christophe-f@users.noreply.github.com>
2016-06-09 13:53:03 -0400

    Update build-artifact.sh

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -1,12 +1,39 @@
 #!/usr/bin/env bash
 
+inputDir=  outputDir=
+ 
+while [ $# -gt 0 ]; do
+  case $1 in
+    -i | --input-dir )
+      inputDir=$2
+      shift
+      ;;
+    -o | --output-dir )
+      outputDir=$2
+      shift
+      ;;
+    * )
+      echo "Unrecognized option: $1" 1>&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 
-cd music-repo
+cd $inputDir
 echo "version"
 echo $version
 gradle assemble -PVersion=$version
 ls build/libs/
+
+
+# Copy jar file to concourse output folder
+cd ..
+cp $inputDir/build/libs/spring-music-1.0.1.war $outputDir/spring-music-1.0.1.war
+echo "file copied"
+ls $outputDir/
