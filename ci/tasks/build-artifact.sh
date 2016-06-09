4f9f9af08dafacd707b7168153677a144ce23607
christophe-f <christophe-f@users.noreply.github.com>
2016-06-09 11:25:37 -0400

    Update build-artifact.sh

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -3,9 +3,11 @@ set -e
 
 export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
+output=`cat build-artifact`
 cd music-repo
-#echo $version
+echo $version
+echo $output
 gradle assemble -PVersion=$version
 ls build/libs/
 
-cp build/libs/spring-music-$version.war build-artifact/spring-music-$version.war
+cp build/libs/spring-music-$version.war $output/spring-music-$version.war
