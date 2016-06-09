69c41ed51e48ed782a7a7c2df379d55821482f96
christophe-f <christophe-f@users.noreply.github.com>
2016-06-09 11:39:53 -0400

    Update build-artifact.sh

--- ci/tasks/build-artifact.sh
+++ ci/tasks/build-artifact.sh
@@ -1,6 +1,23 @@
 #!/usr/bin/env bash
-set -e
 
+
+outputDir=
+ 
+while [ $# -gt 0 ]; do
+  case $1 in
+    -o | --output-dir )
+      outputDir=$2
+      shift
+      ;;
+    * )
+      echo "Unrecognized option: $1" 1>&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+set -e
 export GRADLE_OPTS=-Dorg.gradle.native=false
 version=`cat version/number`
 
@@ -10,4 +27,4 @@ ls `build-artifact`
 gradle assemble -PVersion=$version
 ls build/libs/
 
-cp build/libs/spring-music-$version.war build-artifact/spring-music-$version.war
+cp build/libs/spring-music-$version.war $outputDir/spring-music-$version.war
